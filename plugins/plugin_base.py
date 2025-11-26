a7e8fa1159a5c394657ea420aa4907601ba4882b
Heise, Daniel, RET-D <d.heise@rheinenergie-trading.com>
2025-11-26 21:46:49 +0100

    chore: Entferne unnötige Docstrings in der Plugin-Basisklasse

--- plugins/plugin_base.py
+++ plugins/plugin_base.py
@@ -11,14 +11,8 @@ class PluginBase(ABC):
 
     @abstractmethod
     def get_metric_type(self) -> type:
-        """
-        Liefert den Typ der Metrik zurück (z. B. bool, int, float, str).
-        """
         pass
 
     @abstractmethod
     def get_default_sleep(self) -> int:
-        """
-        Liefert einen Standard-Intervallwert (in Sekunden) als Integer zurück.
-        """
         pass
